abef1538079ae4f587bed34b3d16bdd91f2e72be
Gioco-Ciccione <Danielel2005@gmail.com>
2026-05-14 12:46:21 +0200

    first push

---
+++ main_draft.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cmath>
+
+#include "CShape.h"
+#include "CRectangle.h"
+#include "CRhombus.h"
+#include "CIsoscelesTriangle.h"
+#include <memory.h>
+
+using namespace std;
+
+#define MAX_SHAPES 3
+
+int main()
+{
+    unique_ptr<Shape> figures[MAX_SHAPES];
+
+    figures[0] = make_unique<Rectangle>(0,0,10,30);
+    figures[1] = make_unique<Rectangle>(0,0,10,30);
+    figures[2] = make_unique<IsoscelesTriangle>(12, 67, 67, 67);
+
+    cout << (figures[0] == figures[1]) << endl;
+    
+    return 0;
+}
